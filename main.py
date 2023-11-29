728fd5a9fc06c1a222e6e5d78ed7b03c763ae1c5
Anuroop k jeevan <anuroopkj387@gmail.com>
2023-11-29 22:10:55 +0530

    made changes

--- main.py
+++ main.py
@@ -14,39 +14,30 @@ class ScreenshotApp:
         self.app_name_entry = tk.Entry(self.master)
         self.app_name_entry.pack()
 
-        tk.Label(self.master, text="Unique Identifier:").pack()
-        self.unique_id_entry = tk.Entry(self.master)
-        self.unique_id_entry.pack()
+        tk.Label(self.master, text="Phone:").pack()  # Changed label to "Phone"
+        self.phone_entry = tk.Entry(self.master)     # Changed entry variable to "phone_entry"
+        self.phone_entry.pack()
 
-        # Create a button to take a screenshot and upload
         tk.Button(self.master, text="Take Screenshot", command=self.take_and_upload).pack()
 
     def take_and_upload(self):
-        # Get the application name and unique identifier from the entry widgets
         app_name = self.app_name_entry.get()
-        unique_id = self.unique_id_entry.get()
+        phone = self.phone_entry.get()  # Changed variable name to "phone"
 
-        if not app_name or not unique_id:
-            messagebox.showerror("Error", "Please enter Application Name and Unique Identifier.")
+        if not app_name or not phone:
+            messagebox.showerror("Error", "Please enter Application Name and Phone.")
             return
 
         try:
-            # Take a screenshot
             screenshot = ImageGrab.grab()
-
-            # Save the screenshot locally
             screenshot_path = "screenshot.png"
             screenshot.save(screenshot_path)
 
-            # Prepare data for API request
             api_url = "https://trogon.info/interview/python/index.php"
             files = {'image': open(screenshot_path, 'rb')}
-            data = {'remarks': app_name, 'phone': unique_id}
+            data = {'remarks': app_name, 'phone': phone}  # Changed variable name to "phone"
 
-            # Make the API request
             response = requests.post(api_url, files=files, data=data)
-
-            # Process the API response
             self.handle_api_response(response)
 
         except Exception as e:
@@ -54,7 +45,6 @@ class ScreenshotApp:
 
     def handle_api_response(self, response):
         try:
-            # Parse the JSON response
             api_response = response.json()
 
             if api_response["status"] == "success":
